a9f79ac4784c56f7f2c244888e11310ca6cb303e
servindc <servindc@cimat.mx>
2022-10-12 21:05:25 -0500

    Update 10-12 | update script

--- calculate_slopes.py
+++ calculate_slopes.py
@@ -70,24 +70,27 @@ def plotly_df_slopes(df, csvfile, title=""):
                         horizontal_spacing = 0.02, vertical_spacing = 0.03)
     # plot dataframe columns:
     for i in range(n):
-        z = df.iloc[:,i]
-        s, m, a, b = df_slopes.iloc[i,:] # spike, min, ax+b
-        line = z[int(s):int(m)]
-        def f(x): return (a)*x+b
-        fig.add_trace(go.Scatter(x=df.index, y=z, line=dict(color="#0069b9"), ),
-                                row=i//n_cols+1, col=i%n_cols+1 )
-        # linear fit
-        z = f(line.index)
-        w = line.index
-        fig.add_trace(go.Scatter(x=w, y=z, line=dict(color="#cc0000"), ),
-                    row=i//n_cols+1, col=i%n_cols+1,
-                    )
-        
-        fig.add_annotation(dict(x=w[2*len(w)//3], y=z[0],
-                                text=f"m = {a:.2f}", showarrow=False),
-                                font = dict(color='#cc0000', size=14),
-                                row=i//n_cols+1, col=i%n_cols+1,)
-    
+        try:
+            z = df.iloc[:,i]
+            s, m, a, b = df_slopes.iloc[i,:] # spike, min, ax+b
+            line = z[int(s):int(m)]
+            def f(x): return (a)*x+b
+            fig.add_trace(go.Scatter(x=df.index, y=z,
+                                     line=dict(color="#0069b9"), ),
+                          row=i//n_cols+1, col=i%n_cols+1 )
+            # linear fit
+            z = f(line.index)
+            w = line.index
+            fig.add_trace(go.Scatter(x=w, y=z, line=dict(color="#cc0000"), ),
+                        row=i//n_cols+1, col=i%n_cols+1,
+                        )
+            
+            fig.add_annotation(dict(x=w[2*len(w)//3], y=z[0],
+                                    text=f"m = {a:.2f}", showarrow=False),
+                                    font = dict(color='#cc0000', size=14),
+                                    row=i//n_cols+1, col=i%n_cols+1,)
+        except:
+            print(f"ERROR plotly_df_slopes(): while adding trace number {i}")
     fig.update_layout(title_text="<b>"+title+"</b>", showlegend=False,
                     height=350*n_rows, width=350*n_cols, # 5*n_cols, 4*n_rows
                     margin=dict(l=20, r=20, t=80, b=20),
@@ -157,9 +160,7 @@ if __name__ == "__main__":
         print("ERROR: Search for indexes not implemented;\n",
               "       provide csv file with indexes.\n")
         sys.exit()
-    
-    # Calculate linear curve coeficients
-    
+        
     # Make lists with linear fit parameters ( a*x + b )
     slopes, y_inter = find_slope_all(df, idx_1, idx_2)    
     
@@ -174,14 +175,4 @@ if __name__ == "__main__":
     fig.write_html(file=new_html, auto_play=False)
     print(f"INFO {script}: New file created - '{new_html}'")
     
-    
     sys.exit()
-
-
-indexes = pd.read_csv("../data/trazos ideales_idx.csv")
-df = read_csv_dropcol("../data/trazos ideales.csv")
-
-slopes, y_inter = find_slope_all(df, idx_1, idx_2)    
-
-out_prefix = "../data/trazos ideales"
-csvfile = create_csv_slopes(out_prefix, idx_1, idx_2, slopes, y_inter)
