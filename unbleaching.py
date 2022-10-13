55f69817a0575bd72b710c313f9adafb4e732c56
servindc <servindc@cimat.mx>
2022-10-12 21:05:48 -0500

    Update 10-12 | created unbleaching.py

---
+++ unbleaching.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: servindc
+"""
+
+
+
+################################################################################
+
+if __name__ == "__main__":
+    import sys
+    from argparse import ArgumentParser
+    
+    print("")
+
+    usage = (
+        "%(prog)s datafile.csv [-h] [-i] [-o] [-d]"
+        )
+
+    sys.exit()
