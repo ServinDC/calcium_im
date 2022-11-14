ce4cf5ad1daea5d4b5a78311802a02c675950dca
servindc <servindc@cimat.mx>
2022-11-14 06:07:46 -0600

    Fixed roi related scripts

--- check_rois.py
+++ check_rois.py
@@ -81,36 +81,3 @@ if __name__ == "__main__":
     print(f"INFO Area changed in {c}%\n")
     
     sys.exit()
-
-# TESTS
-
-inner_roi = "../data/ROIS3/monocapa/UTP/ROIs/ROI_UTP[-8]_201119_3_nucleus/n10_0001-0156-0272.roi"
-outer_roi = "../data/ROIS3/monocapa/UTP/ROIs/ROI_UTP[-8]_201119_3_wholecell/c10_0001-0156-0273.roi"
-
-inner_path = Path(inner_roi)
-outer_path = Path(outer_roi)
-
-#---
-
-outer_pol = outer_pol.buffer(0)
-
-x, y = outer_pol.boundary.xy
-new_out = ImagejRoi.frompoints( np.array([x,y]).T )
-new_out.tofile(filename=outer_roi)
-
-
-
-inner_pol = inner_pol.buffer(0)
-
-
-
-
-i = np.argmax([ g.area for g in outer_pol.geoms ])
-outer_pol = outer_pol.geoms[i]
-
-
-
-
-x,y = new_inner.boundary[0].xy
-
-
